ce02792eba31651a005045beb7d2f260ff9d0c73
Vinicius <viniciuspereiraassuncao@gmail.com>
2026-01-26 18:38:07 -0300

    Correcting the fold name

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,17 +1,59 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { api } from "../services/api";
+import { z, ZodError } from "zod"
 
 import Pencil from "../assets/icons/pen-line.svg";
 import { Button } from "../components/Button";
-import plus from "../assets/icons/inative/plus1.svg";
-import ban from "../assets/icons/inative/ban.svg"
-import check from "../assets/icons/inative/circle-check.svg"
+import Plus from "../assets/icons/inactive/plus1.svg";
+import Ban from "../assets/icons/inactive/ban.svg"
+import Check from "../assets/icons/inactive/circle-check.svg"
+import { AxiosError } from "axios";
+
+const ServiceSchema = z.object({
+    status: z.enum(["active", "inactive"]).optional(),
+})
 
 export function Services() {
     const [service, setServices] = useState<Service[]>([]);
     const { session, isLoading } = useAuth();
     // const navigate = useNavigate();
+
+    async function Active(name: string) {
+        try {
+            const data = ServiceSchema.parse(
+                status = "active"
+            )
+
+            await api.patch(`/services/${name}`, data)
+            console.log("Updated sucessfully")
+        } catch (error) {
+            console.log(error)
+            
+            if(error instanceof ZodError){
+                return { message: error.issues[0].message}
+            }
+
+            if(error instanceof AxiosError){
+                return { message: error.response?.data.message}
+            }
+
+            return { message: "Não foi possível entrar!" }
+        }
+    }
+
+    async function Inactive(name: string) {
+        try {
+            const data = ServiceSchema.parse(
+                status = "inactive"
+            )
+
+            await api.patch(`/services/${name}`, data)
+            console.log("Updated sucessfully")
+        } catch (error) {
+            console.error("Error updating", error)
+        }
+    }
 
     useEffect(() => {
         if (isLoading) return;
@@ -19,7 +61,6 @@ export function Services() {
 
         api.get("/services/").then((response) => {
         setServices(response.data);
-        console.log(service);
         });
         }, [isLoading, session]);
 
@@ -28,7 +69,7 @@ export function Services() {
             <div className="flex justify-between">
                 <h1 className="text-xl text-blue-dark font-semibold mb-4">Serviços</h1>
                 <Button className="w-fit h-fit py-3 px-3">
-                    <img src={plus} alt="" />
+                    <img src={Plus} alt="" />
                     Novo
                 </Button>
                 </div>
@@ -39,36 +80,36 @@ export function Services() {
                             <tr className="text-sm text-gray-400 border-b border-gray-500">
                                 <th className="px-7 py-2 text-left">Nome</th>
                                 <th className="px-7 py-2 text-left">Valor</th>
-                                <th className="px-60 py-2 text-left">Status</th>
+                                <th className="px-7 py-2 text-left">Status</th>
                             </tr>
                         </thead>
 
                         <tbody>
                             {service.map((service) => (
                             <tr className="border-b border-gray-500 last:border-b-0">
-                                <td className="px-7 py-2 text-xs text-gray-200">
+                                <td className="px-7 py-1 text-xs text-gray-200">
                                     {service.name}
                                 </td>
-                                <td className="px-7 py-2 text-xs text-gray-200 ">
+                                <td className="px-7 py-1 text-xs text-gray-200 ">
                                     R$ {service.amount.toFixed(2)}
                                 </td>
-                                <td className="px-60 py-2">
-                                <div className={`flex justify-center items-center gap-1 px-1 py-1.5 text-xs text-gray-200 font-medium rounded-xl ${service.status === "inative" ? " bg-red-200 text-red-800" : "bg-green-200 text-green-800"} `}>
+                                <td className="px-7 py-1">
+                                <div className={`flex justify-center items-center gap-1 px-1 py-1.5 text-xs text-gray-200 font-medium rounded-xl ${service.status === "inactive" ? " bg-red-200 text-red-800" : "bg-green-200 text-green-800"} `}>
                                     {service.status === "active" ? "Ativo" : "Inativo"}
                                 </div>
                                 </td>
                                 <td className="px-7 py-2 ">
                                     {service.status === "active" ?
-                                    <button className="flex gap-2 text-xs text-gray-300">
-                                        <img src={ban} alt="" />
+                                    <Button onClick={() => Inactive(service.name)} className="flex gap-2 text-xs text-gray-300 bg-transparent border-none hover:bg-transparent">
+                                        <img src={Ban} alt="" />
                                         Desativar
-                                    </button>
+                                    </Button>
                                 :
-                                    <button className="flex gap-2 text-xs text-gray-300">
-                                        <img src={check} alt="" />
+                                    <Button onClick={() => Active(service.name)} className="flex gap-2 text-xs text-gray-300 bg-transparent border-none hover:bg-transparent">
+                                        <img src={Check} alt="" />
                                         Reativar
-                                    </button>
-                                }
+                                    </Button>
+                                }   
 
                                 </td>
 
